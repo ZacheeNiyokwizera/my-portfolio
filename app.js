adf38b9bc0c0c32586d021b34a91cf20bfd5081a
unknown <zackniyokwizera>
2021-06-04 18:12:42 +0200

    added GA

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ firebase.initializeApp(firebaseConfig);
 var messagesRef = firebase.database().ref("messages");
 
 // Listen for form submit
-document.getElementById("contactForm").addEventListener("submit", submitForm);
-console.log("clicked");
+// document.getElementById("contactForm").addEventListener("submit", submitForm);
+// console.log("clicked");
 
 // Submit form
 function submitForm(e) {
@@ -41,7 +41,9 @@ function submitForm(e) {
     }, 3000);
 
     // Clear form
-    document.getElementById("contactForm").reset();
+    // document.getElementById("contactForm").reset();
+
+    return false;
 }
 
 // Function to get get form values
@@ -89,52 +91,3 @@ menu_item.forEach((item) => {
         mobile_menu.classList.toggle("active");
     });
 });
-
-filterSelection("all");
-
-function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("column");
-    if (c == "all") c = "";
-    for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-    }
-}
-
-// filter selection for Languages
-
-function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {
-            element.className += " " + arr2[i];
-        }
-    }
-}
-// filter selection for Languages
-
-function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-        }
-    }
-    element.className = arr1.join(" ");
-}
-
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
